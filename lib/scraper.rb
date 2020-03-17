7f701b0a0a25c7281f8a2158f8450458bb7aec83
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-03-17 03:28:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,8 +25,8 @@ class Scraper
           course.schedule = post.css(".date").text
           course.description = post.css("p").text
         end
-  #  binding.pry
-  end
   binding.pry
+  end
+
 end
 Scraper.new.get_page
